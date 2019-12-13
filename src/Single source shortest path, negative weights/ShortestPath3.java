580507420298571e31f2f8db95adea21a4306c10
Teng Le <zenatrick@gmail.com>
2019-12-13 18:49:34 +0800

    Add solutions to two SSSP graph problem
    Use my own SSSPGraph class for solving standard SSSP problems using
    Dijkstra's and Bellman-Ford algorithm to solve the following problems.
    
    1. Single source shortest path, non-negative weights [shortestpath1]
        - Perform Dijkstra's algorithm.
    2. Single source shortest path, negative weights [shortestpath3]
        - Perform Bellman-Ford algorithm.

---
+++ src/Single source shortest path, negative weights/ShortestPath3.java
@@ -0,0 +1,52 @@
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+class ShortestPath3 {
+    static double INF = SSSPGraph.INF;
+
+    public static void main(String[] args) throws Exception {
+        PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out));
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            String[] data = in.readLine().split("\\s");
+            int nodes = Integer.parseInt(data[0]);
+            int edges = Integer.parseInt(data[1]);
+            int queries = Integer.parseInt(data[2]);
+            int source = Integer.parseInt(data[3]);
+
+            // Check for termination condition
+            if (nodes == 0) {
+                break;
+            }
+
+            // Initialise SSSPGraph
+            SSSPGraph graph = SSSPGraph.initGraphForSSSPDist(nodes, edges);
+
+            // Read and add edges to graph
+            for (int i = 0; i < edges; i++) {
+                String[] edgeData = in.readLine().split("\\s");
+                int u = Integer.parseInt(edgeData[0]);
+                int v = Integer.parseInt(edgeData[1]);
+                int w = Integer.parseInt(edgeData[2]);
+                graph.addEdge(u, v, w);
+            }
+
+            // Perform Bellman-Ford
+            graph.bellmanFordWithoutPath(source);
+
+            // Print result of queries
+            for (int i = 0; i < queries; i++) {
+                int qnode = Integer.parseInt(in.readLine());
+                double distance = graph.getDistOf(qnode);
+                out.println(distance == INF ? "Impossible" : distance == -INF ? "-Infinity" : (int) distance);
+            }
+            out.println();
+        }
+
+        in.close();
+        out.close();
+    }
+}
